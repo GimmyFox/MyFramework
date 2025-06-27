da72a56d14ea6dd9853d6bb0477f8cf7836d8d1e
m.guzeev <m.guzeev@iva.ru>
2025-06-27 22:42:06 +0300

    public init added

--- Sources/MyFramework/ContentView.swift
+++ Sources/MyFramework/ContentView.swift
@@ -3,6 +3,12 @@ import SwiftUI
 public struct GreetingView: View {
   public let name: String
   public let surname: String
+  
+  public init(name: String, surname: String) {
+    self.name = name
+    self.surname = surname
+  }
+  
   public var body: some View {
     VStack {
       Text("Hello, \(name)!")
