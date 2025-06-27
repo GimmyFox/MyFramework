c36de843cfa001f66f936f2ae1ac289727131630
m.guzeev <m.guzeev@iva.ru>
2025-06-27 22:37:22 +0300

    surname added

--- Sources/MyFramework/ContentView.swift
+++ Sources/MyFramework/ContentView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 public struct GreetingView: View {
   public let name: String
+  public let surname: String
   public var body: some View {
     VStack {
       Text("Hello, \(name)!")
@@ -11,5 +12,5 @@ public struct GreetingView: View {
 }
 
 #Preview {
-  GreetingView(name: "Maks")
+  GreetingView(name: "Maks", surname: "...")
 }
